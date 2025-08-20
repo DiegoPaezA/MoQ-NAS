345b5a247ac83f4774c85631759a7ea4ad8d1e40
DiegoPaezA <diripar8@gmail.com>
2025-08-20 11:52:22 -0300

    refactor: update run.sh configuration for experiment parameters and execution logic

--- run.sh
+++ run.sh
@@ -7,13 +7,16 @@ data_path="${dataset}_data"
 log_level="INFO"
 network_config="default"
 backbone_name="resnet18"
-use_cache=true  # Use cached evaluations to speed up runs
+use_cache=false  # Use cached evaluations to speed up runs
+early_stopping=false
+en_pop_crossover=true
+elite_mode="bootstrap_k" # "single" | "global_k" | "bootstrap_k" | "old"
 
 dataset_sample_size=10000
 
-configs=("config0.txt")
-exps=("exp6")
-cuda_devices=("1")
+configs=("config5.txt")
+exps=("exp12")
+cuda_devices=("0")
 
 # Loop over the length of the configs array
 for ((j=0; j<${#configs[@]}; j++)); do
@@ -23,7 +26,7 @@ for ((j=0; j<${#configs[@]}; j++)); do
 
     echo "Running evolution experiment with $config"
 
-    for ((i=2; i<=3; i++)); do # Change the range to the number of repeats
+    for ((i=1; i<=1; i++)); do # Change the range to the number of repeats
         exp_path="${exp_path_base}/${exp}_repeat_$i"
 
         CUDA_VISIBLE_DEVICES="$cuda_device" python run_evolution.py \
@@ -33,11 +36,12 @@ for ((j=0; j<${#configs[@]}; j++)); do
             --dataset "$dataset" \
             --limit_data_value "$dataset_sample_size" \
             --fitness_metric "$fitness_metric" \
-            --early_stopping \
+            $($early_stopping && echo "--early_stopping")\
             --network_config "$network_config" \
             --backbone_name "$backbone_name" \
-            --en_pop_crossover \
+            $($en_pop_crossover && echo "--en_pop_crossover")\
             $($use_cache && echo "--use_cache")\
+            --elite_mode "$elite_mode" \
             --log_level "$log_level"
     done
 done
