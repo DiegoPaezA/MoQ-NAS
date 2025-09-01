7e3b59e99898800af8bd70293cd10e1583764075
DiegoPaezA <diripar8@gmail.com>
2025-09-01 18:00:24 -0300

    feat: load objectives configuration from JSON and set objective directions in MOQNAS

--- moqnas.py
+++ moqnas.py
@@ -8,6 +8,7 @@ hyperparameter distributions.
 """
 import os
 import time
+import json
 import pickle
 import numpy as np
 from qnas2 import QNAS
@@ -136,7 +137,32 @@ class MOQNAS(QNAS):
         self.pareto_global_params = None
         self.pareto_global_ids = []
         self.fronts_history = {}
+        
+        # load config objective sense
+        try:
+            with open("config_objectives/cfg_obj.json", "r") as f:
+                self.objectives_info = json.load(f)["objectives"]
+        except Exception as e:
+            print(f"Error loading objectives config: {e}")
 
+        objective_names = []
+        objective_senses = []
+
+        # Map the active objectives to their information
+        for active_obj in self.objectives:
+            match_found = False
+            for key, info in self.objectives_info.items():
+                if key in active_obj:
+                    objective_names.append(key)
+                    sense = 'max' if info['goal'] == 'maximize' else 'min'
+                    objective_senses.append(sense)
+                    match_found = True
+                    break # Move to the next active_obj
+            if not match_found:
+                print(f"Warning: Could not find a rule for '{active_obj}'")
+        
+        # moead_topk
+        self.qpop_net.set_objective_directions(names=objective_names, sense=objective_senses)
 
     def multiobjective_fitness(self) -> np.ndarray:
         """
@@ -472,12 +498,14 @@ class MOQNAS(QNAS):
             # We use the crowding distance that was calculated and stored in the previous step.
             cd = self._last_cd
             sorted_rel = np.argsort(cd)[::-1]
-            pick = sorted_rel[:self.qpop_net.num_ind]
+            # pick = sorted_rel[:self.qpop_net.num_ind]
 
             # 4. Set the chosen individuals as the 'parents' for the quantum update.
-            self.qpop_net.current_pop = self.pareto_global_population[pick]
+            self.qpop_net.current_pop = self.pareto_global_population[sorted_rel]
+            self.qpop_net.current_pop_objs = self.pareto_global_fitnesses[sorted_rel]
+            
             if self.pareto_global_params is not None:
-                self.qpop_params.current_pop = self.pareto_global_params[pick]
+                self.qpop_params.current_pop = self.pareto_global_params[sorted_rel]
 
             # 5. Trigger the quantum population update (the learning step).
             self.update_quantum(self.current_gen)
